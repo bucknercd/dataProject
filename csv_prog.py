6f68d61d56be1876a08d925ca690579f96bf7362
Chris Buckner <christopher.d.buckner@gmail.com>
2015-11-01 22:55:12 -0600

    commit

--- csv_prog.py
+++ csv_prog.py
@@ -8,34 +8,42 @@ fcr1 = 'FCRALLDetailReportNathanBonnet.csv'
 fcr2 = 'FCRALLDetailReortMarissaLopez.csv'
 
 f1 = open(rpt1, 'r')
-csv_obj = csv.reader(f1)
+csv_obj1 = csv.reader(f1)
 
 accts = []
 
-for row in csv_obj:
+for row in csv_obj1:
    try:
       accts.append(row[10])
    except:
       print 'end of csv file'
 f1.close()
+accts.pop(0)
+
 f2 = open('test.txt', 'w')
 for i in accts:
    f2.write(i)
 f2.close()
 
-report = open('report.txt', 'w')
+report = open('report.txt', 'a')
 
 f2 = open(fcr2, 'r')
-csv_obj = csv.reader(f2)
+csv_obj2 = csv.reader(f2)
+dic = {}
 
-for row in csv_obj:
+'''for row in csv_obj2:
    try:
-      acct_num = row[7]
+      acct = row[7]
       name = row[3]
+      dic[acct] = name
    except:
       print 'end of 2nd csv file'
-   if acct_num in accts:
-      info = name +'\t'+acct_num
-      report.write(info)
+      time.sleep(20)
+
+
+for i in accts:
+   info = dic[i]+'\t\t'+i'\n'
+   report.write(info)'''
+   
 
 
