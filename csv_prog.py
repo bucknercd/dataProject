c7b9a1f6d8d0b41ce1dc953a5070725a72907f97
Chris Buckner <christopher.d.buckner@gmail.com>
2015-11-01 22:00:54 -0600

    modified prog to make a report file

--- csv_prog.py
+++ csv_prog.py
@@ -6,14 +6,36 @@ rpt2 = 'RptCareTCDetailDanielKinney.csv'
 
 fcr1 = 'FCRALLDetailReportNathanBonnet.csv'
 fcr2 = 'FCRALLDetailReortMarissaLopez.csv'
-r = csv.reader(open(rpt1,'r'))
-items = []
-next(r, None)
-for row in r:
-   items.append(row[10])
 
-print items
-time.sleep(2)
-f = open('test.txt', 'w')
-f.write(items)
+f1 = open(rpt1, 'r')
+csv_obj = csv.reader(f1)
+
+accts = []
+
+for row in csv_obj:
+   try:
+      accts.append(row[10])
+   except:
+      print 'end of csv file'
+f1.close()
+f2 = open('test.txt', 'w')
+for i in accts:
+   f2.write(i)
+f2.close()
+
+report = open('report.txt', 'w')
+
+f2 = open(fcr2, 'r')
+csv_obj = csv.reader(f2)
+
+for row in csv_obj:
+   try:
+      acct_num = row[7]
+      name = row[3]
+   except:
+      print 'end of 2nd csv file'
+   if acct_num in accts:
+      info = name +'\t'+acct_num
+      report.write(info)
+
 
